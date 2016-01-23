cc25581c44b5575d00f2002cd4de1b348b1ed15b
Neil Murphy <wnmurphy@gmail.com>
2016-01-23 08:38:22 -0800

    Enemy movement working without crashing.

--- client/watchout.js
+++ client/watchout.js
@@ -1,60 +1,90 @@
+// http://latentflip.com/LearningD3/collider/
+
 //============================================================
 // Set up SVG game area
 var board = 
-d3.select("body")
-.append("svg")
-.attr("position", "absolute")
-.attr("width", 500)
-.attr("height", 500);
-
+  d3.select("body")
+    .append("svg")
+    .attr("position", "absolute")
+    .attr("width", 500)
+    .attr("height", 500);
 
 //============================================================
 //Build enemies [https://developer.mozilla.org/en-US/docs/SVG]
-var enemies = [];
+var enemyArr = [];
 for(var i = 0; i < 11; i++){
-  enemies.push(i);
+  enemyArr.push(i);
 };
 
-board.selectAll('circles')
-.data(enemies)
-.enter()
-.append("circle") // appends circle to each svg
-.attr("cx", function (d, i) { return 50 * (Math.floor(Math.random() * i));}) //circle coordinates within svg
-.attr("cy", function (d, i) { return 50 * (Math.floor(Math.random() * i));}) //circle coordinates within svg
-.attr("r", 5) //circle radius
-.style("fill", "purple");
+var enemies = 
+  board.selectAll('circle') // grab empty reference for circles to fill in 
+    .data(enemyArr)         //plug in to data source to bind to each circle
+    .enter()                //create a placeholder for each datum in data source
+      .append("circle") // appends circle to each svg
+      .attr("cx", function (d, i) { return 50 * (Math.floor(Math.random() * i));}) 
+      .attr("cy", function (d, i) { return 50 * (Math.floor(Math.random() * i));}) 
+      .attr("r", 5) //circle radius
+      .style("fill", "purple")
+      .classed('enemy', true);
 
-//Use CSS3 animations to make the enemies whirling shuriken.
+  //Use CSS3 animations to make the enemies whirling shuriken.
  // [https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Using_CSS_animations]
 
 
 //============================================================
 // Create the player and make him draggable.
-// Add another circle of a new color to board SVG.
-board.selectAll('rects')
-.data([1])
-.enter()
-.append('rect')
-.attr("x", 250) 
-.attr("y", 250) 
-.attr("height", 10) 
-.attr("width", 10)  
-.style("color", "blue")
 
-  // Use D3 to make player draggable.
+//Handle dragging
+/*
+var point = d3.mouse(this);
+var p = {x: point[0], y: point[1] };
+
+ // Initialize drag behavior
+.on("drag", dragmove;
+
+function dragmove(d) {
+  var x = d3.event.x;
+  var y = d3.event.y;
+  d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
+}
+*/
+
+
+// Add a rect of a new color to board SVG.
+var player = 
+  board.selectAll('rects')
+    .data([1])
+    .enter()
+      .append('rect')
+      .attr("x", 250) 
+      .attr("y", 250) 
+      .attr("height", 10) 
+      .attr("width", 10)  
+      .style("color", "blue")
+      .classed('player', true);
+
+player.on("click", function(){ alert("derp");});
+
+var drag = d3.behavior.drag();
+
+//player.on("click", function() {
+//  player.call(drag);
+//});
 
 
 //============================================================
 // Function to handle movement of enemies and collision detection
-var movement = function(){
+var moveEnemies = function(){
+  enemies
+    .transition()
+    .duration(2000)
+    .attr("cx", function (d, i) { return 50 * (Math.floor(Math.random() * i));}) 
+    .attr("cy", function (d, i) { return 50 * (Math.floor(Math.random() * i));});
 
-  // Update coordinates of each enemy to randomize x and y every second
-    //setInterval()
-  d3.selectAll('enemy')
-  .attr("cx", function(d, i){return Math.random() * i})
-  .attr("cy", function(d, i){return Math.random() * i});
+  window.setTimeout(moveEnemies, 2000);
+};
 
-    // Once enemies are moving, use d3.transition() to animate smoothly
+
 
   // Set up event listener for player proximity to enemy
 
@@ -67,7 +97,6 @@ var movement = function(){
 
       // increment collisions
 
-};
   
 //============================================================
 // On load, begin incrementing currentScore
@@ -81,9 +110,9 @@ var scoreCount = function(){
     return updateCurrentScore();
   }
 };
+
 scoreCount();
-
-
+moveEnemies();
 
 
 
